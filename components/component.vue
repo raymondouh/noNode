fd6b8e24b8bfc85e526f8e4895fab4b224cc6924
Andrei Serbin <serbinandr@gmail.com>
2021-08-15 17:59:52 +0300

    added component loading from other component

--- components/component.vue
+++ components/component.vue
@@ -3,12 +3,16 @@
         <span>
             {{text}}
         </sapn>
+        <hello3></hello3>
     </div>
 </template>
 
 <script>
     module.exports = {
         name: "hello",
+        components: {
+          hello3: httpVueLoader('components/treeComponent/test2.vue')
+      },
         data() {
             return {
                 text: "hello world"
