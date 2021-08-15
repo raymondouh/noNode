5cd8e5b0f190ef98f0f8fca80097fe7e3c051791
Andrei Serbin <serbinandr@gmail.com>
2021-08-15 17:49:01 +0300

    vue components no node.js

---
+++ components/component.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <span>
+            {{text}}
+        </sapn>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        name: "hello",
+        data() {
+            return {
+                text: "hello world"
+            }
+        }
+    }
+</script>
